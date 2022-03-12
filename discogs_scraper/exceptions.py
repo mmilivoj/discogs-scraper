d08e632a25b4e5ee94956ec7d77b6246b99d47c1
Marko Milivojevic <mmilivoj@stud.hs-heilbronn.de>
2022-03-12 23:04:20 +0100

    Adapt code to flake8

--- discogs_scraper/exceptions.py
+++ discogs_scraper/exceptions.py
@@ -1,13 +1,13 @@
 """Exceptions for discogs-scraper."""
 
+
 class NetworkCallFailed(Exception):
     """Raise error if reading url fails due to connectivity issues."""
+
 
 class EmptyTitle(Exception):
     """Raise error if album title is empty string."""
 
+
 class NoArtistForAlbum(Exception):
     """Raise if no artist was found for album title."""
-
-    
-
